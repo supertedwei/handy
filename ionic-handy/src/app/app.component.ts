015e6cb5d9b64adb5dc560f59aec4012ae468189
Ted Wei <jdwei@hotmail.com>
2017-01-16 17:18:21 +0800

    display app version

--- ionic-handy/src/app/app.component.ts
+++ ionic-handy/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
+import { StatusBar, Splashscreen, AppVersion } from 'ionic-native';
 
 import { NoteListPage } from '../pages/note-list/note-list';
 
@@ -14,6 +14,8 @@ export class MyApp {
   rootPage: any = NoteListPage;
 
   pages: Array<{title: string, component: any}>;
+
+  versionNumber = 'not available';
 
   constructor(public platform: Platform) {
     this.initializeApp();
@@ -31,6 +33,17 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
+
+
+      if (this.platform.is('cordova')) {
+        AppVersion.getVersionNumber().then((s) => {
+          this.versionNumber = s;
+          console.log('versionNumber : ' + this.versionNumber);
+        })
+      } else {
+        console.log('not a cordova platform');
+      }
+
     });
   }
 
