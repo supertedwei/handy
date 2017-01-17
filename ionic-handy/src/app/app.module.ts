7cd50fa36af722d4fc56e71eda4ceda1509d7d58
Ted Wei <jdwei@hotmail.com>
2017-01-17 10:32:10 +0800

    add angularfire2

--- ionic-handy/src/app/app.module.ts
+++ ionic-handy/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+
+import { FirebaseConfig } from './settings';
 import { NoteListPage } from '../pages/note-list/note-list';
 
 @NgModule({
@@ -9,7 +14,8 @@ import { NoteListPage } from '../pages/note-list/note-list';
     NoteListPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FirebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
