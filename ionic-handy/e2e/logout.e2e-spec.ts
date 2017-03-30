81dbe8b94c25c9c02f81689d216a318c20c5e5c4
Ted Wei <jdwei@hotmail.com>
2017-03-30 16:02:56 +0800

    add first e2e test case

---
+++ ionic-handy/e2e/logout.e2e-spec.ts
@@ -0,0 +1,54 @@
+import { browser, element, by, ElementFinder, ExpectedConditions } from 'protractor';
+
+describe('Logout Test', () => {
+
+  beforeEach(() => {
+    console.log("beforeEach")
+    browser.get('');
+    //browser.driver.sleep(5000)
+  });
+
+  afterEach(() => {
+    console.log("afterEach")
+    //browser.driver.sleep(5000)
+  })
+
+  it('Test Logout', () => {
+
+    // browser.wait(ExpectedConditions.visibilityOf(element(by.css('[menutoggle]'))), 5000).then(() => {
+    //   element(by.css('[menutoggle]')).click().then(() => {
+    //     browser.driver.sleep(1000)
+    //     element(by.id('menu_settings')).click().then(() => {
+    //       browser.driver.sleep(1000)
+    //       element(by.id('btn_logout')).click().then(() => {
+    //       })
+    //     })
+    //   })
+    // })
+
+  })
+
+  it('Login successfully', () => {
+
+    // browser.driver.actions()
+    //     .mouseDown(element(by.id('email'))).click()
+    //     .sendKeys("jdwei@yahoo.com")
+    //     .mouseDown(element(by.id('password'))).click()
+    //     .sendKeys("111111")
+    //     .perform()
+
+    // element(by.id('btn_login')).click().then(() => {
+    //   console.log("login clicked")
+    //   browser.wait(ExpectedConditions.presenceOf(element(by.css('ion-menu'))), 5000).then(() => {
+    //     console.log("ion-menu found")
+    //     browser.getTitle().then((title) => {
+    //       console.log("title : " + title)
+    //       expect(title).toEqual('Library');
+    //     });
+    //   })
+    // });
+ 
+  });
+
+
+})
---
+++ ionic-handy/protractor.conf.js
@@ -0,0 +1,49 @@
+var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+//import { browser, element, by, ElementFinder, ExpectedConditions } from 'protractor';
+ 
+exports.config = {
+    allScriptsTimeout: 5000000,
+    directConnect: true,
+    capabilities: {
+        'browserName': 'chrome'
+    },
+    framework: 'jasmine',
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000,
+        print: function() {}
+    },
+    specs: ['./e2e/**/*.e2e-spec.ts'],
+    baseUrl: 'http://localhost:8100',
+    useAllAngular2AppRoots: true,
+    beforeLaunch: function() {
+ 
+        require('ts-node').register({
+            project: 'e2e'
+        });
+ 
+        require('connect')().use(require('serve-static')('www')).listen(8100);
+ 
+    },
+    onPrepare: function() {
+        jasmine.getEnv().addReporter(new SpecReporter());
+        browser.get('');
+        browser.driver.actions()
+            .mouseDown(element(by.id('in_email'))).click()
+            .sendKeys("jdwei@yahoo.com")
+            .mouseDown(element(by.id('in_password'))).click()
+            .sendKeys("111111")
+            .perform()
+
+        element(by.id('btn_login')).click().then(() => {
+            console.log("login clicked")
+            browser.wait(ExpectedConditions.presenceOf(element(by.css('ion-menu'))), 5000).then(() => {
+                console.log("ion-menu found")
+                // browser.getTitle().then((title) => {
+                // console.log("title : " + title)
+                // expect(title).toEqual('Library');
+                // });
+            })
+        });
+    }
+}
